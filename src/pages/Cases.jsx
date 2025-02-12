afd01abc5b27750758c4754b2da1e01f3c556bef
nazneen636 <nazneennahar636@gmail.com>
2025-02-12 10:00:06 +0600

    changes

--- src/pages/Cases.jsx
+++ src/pages/Cases.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CasesBanner from "../components/CasesBanner";
-import CasesCategory from "../components/casesCategory";
+// import CasesCategory from "../components/casesCategory";
+import CasesCategory from "../components/CasesCategory";
 import Testimonial from "../components/Testimonial";
 
 const Cases = () => {
